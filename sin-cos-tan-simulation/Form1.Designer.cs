a193f5a8e4ccd52909ba9407414a7b17bda4c296
rs_SlavikK <rs_SlavikK>
2024-01-13 23:24:51 +0100

    work

--- sin-cos-tan-simulation/Form1.Designer.cs
+++ sin-cos-tan-simulation/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             components = new System.ComponentModel.Container();
             panel1 = new Panel();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             label1 = new Label();
             trackBar1 = new TrackBar();
             bindingSource1 = new BindingSource(components);
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
@@ -45,7 +47,9 @@
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(64, 64, 64);
-            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
             panel1.Controls.Add(label4);
             panel1.Controls.Add(label3);
             panel1.Controls.Add(label2);
@@ -57,6 +61,43 @@
             panel1.Size = new Size(683, 434);
             panel1.TabIndex = 0;
             panel1.Paint += panel1_Paint;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(66, 66, 66);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(571, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F);
+            label4.Location = new Point(600, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 20);
+            label4.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(600, 49);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(600, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(9, 20);
+            label2.TabIndex = 2;
+            label2.Text = "\r\n";
             // 
             // label1
             // 
@@ -75,41 +116,22 @@
             trackBar1.Size = new Size(104, 45);
             trackBar1.TabIndex = 0;
             // 
-            // label2
+            // textBox2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(609, 13);
-            label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 2;
-            label2.Text = "label2";
+            textBox2.BackColor = Color.FromArgb(66, 66, 66);
+            textBox2.Location = new Point(571, 41);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
             // 
-            // label3
+            // textBox3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(617, 36);
-            label3.Name = "label3";
-            label3.Size = new Size(38, 15);
-            label3.TabIndex = 3;
-            label3.Text = "label3";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(617, 65);
-            label4.Name = "label4";
-            label4.Size = new Size(38, 15);
-            label4.TabIndex = 4;
-            label4.Text = "label4";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(620, 98);
-            label5.Name = "label5";
-            label5.Size = new Size(38, 15);
-            label5.TabIndex = 5;
-            label5.Text = "label5";
+            textBox3.BackColor = Color.FromArgb(66, 66, 66);
+            textBox3.ForeColor = Color.White;
+            textBox3.Location = new Point(571, 72);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
             // 
             // Form1
             // 
@@ -132,9 +154,11 @@
         private TrackBar trackBar1;
         private BindingSource bindingSource1;
         private Label label1;
-        private Label label5;
-        private Label label4;
-        private Label label3;
-        private Label label2;
+        public Label label4;
+        public Label label3;
+        public Label label2;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
     }
 }
--- sin-cos-tan-simulation/Form1.cs
+++ sin-cos-tan-simulation/Form1.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace sin_cos_tan_simulation
 {
@@ -13,53 +14,80 @@ namespace sin_cos_tan_simulation
         SolidBrush _brCos = new SolidBrush(Color.LawnGreen);
         SolidBrush _brTan = new SolidBrush(Color.Orange);
 
+        float sin, cos;
+
         public Form1()
         {
             InitializeComponent();
             typeof(Panel).InvokeMember("DoubleBuffered",
-            BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
-            null, panel1, new object[] { true });
+                    BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                    null, panel1, new object[] { true });
+
+            
+            textBox1.ForeColor = Color.White;
+            textBox2.ForeColor = _brSin.Color;
+            textBox3.ForeColor = _brCos.Color;      
         }
 
-        private void RegisterEventHandler()
+        /*private void RegisterEventHandler()
         {
             panel1.SizeChanged += new EventHandler(this.panel1_SizeChanged);
+
         }
 
         private void panel1_SizeChanged(object sender, System.EventArgs e)
         {
             panel1.Invalidate();
-        }
-
-        void Draw_Simulation(Graphics gr)
-        {
-            currWidth = panel1.Size.Width;
-            currHeight = panel1.Size.Height;
-
-            Draw_Grid(gr);
-
-            Draw_Sin(gr);
-            Draw_Cos(gr);
-            Draw_Tan(gr);           
-        }
-
-        
+            
+        }*/
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             Draw_Simulation(e.Graphics);
-            RegisterEventHandler();
+            //RegisterEventHandler();
             angle++;
-            if (angle >= 360)
+            //label2.Text = $"Angle: {angle}\nSin: {sin}\nCos: {cos}";
+            textBox1.Text = $"Angle: {angle}";
+            textBox2.Text = $"Sin: {sin}";
+            textBox3.Text = $"Cos: {cos}";
+            if (angle == 360)
                 angle = 0;
-            Wait(10);
+
+            Wait(20);
             panel1.Invalidate();
         }
 
-        double To_Radiant(int degree)
+        void Draw_Simulation(Graphics gr)
+        {
+            currWidth = panel1.Width;
+            currHeight = panel1.Height;
+
+            
+            Draw_Grid(gr);
+
+            Draw_Sin(gr);
+            Draw_Cos(gr);
+            Draw_Circle(gr);
+           
+            Point location = new Point(currWidth, currHeight);
+            location.X = currWidth - 100;
+            location.Y = currHeight - 30;
+
+            /*this.label2.Location = location;
+            label2.Refresh();
+            location.Y -= 30;
+            this.label3.Location = location;
+            label3.Refresh();
+            location.Y -= 30;
+            this.label4.Location = location;
+            label4.Refresh();*/
+            
+        }
+
+        float To_Radiant(int degree)
         {
             double radiant = degree * (Math.PI/180);
-            return radiant;
+            return (float)radiant;
         }
 
         private void Draw_Grid(Graphics gr)
@@ -74,20 +102,20 @@ namespace sin_cos_tan_simulation
 
         void Draw_Sin(Graphics gr)
         {
-            float sin = (float)Math.Sin(To_Radiant(angle));
+            sin = (float)Math.Round(Math.Sin(To_Radiant(angle)), 3);
             gr.FillRectangle(_brSin, currWidth / 2 -180 * sin - width / 2, currHeight / 2 - height / 2, width, height);
         }
 
         void Draw_Cos(Graphics gr)
         {
-            float cos = (float)Math.Cos(To_Radiant(angle));
+            cos = (float)Math.Round(Math.Cos(To_Radiant(angle)), 3);
             gr.FillRectangle(_brCos, currWidth / 2 - width / 2, currHeight / 2 - 180 * cos - height / 2, width, height);
         }
 
-        void Draw_Tan(Graphics gr)
+        void Draw_Circle(Graphics gr)
         {
-            float sin = (float)Math.Sin(To_Radiant(angle));
-            float cos = (float)Math.Cos(To_Radiant(angle));
+            float sin = (float)Math.Round(Math.Sin(To_Radiant(angle)), 3);
+            float cos = (float)Math.Round(Math.Cos(To_Radiant(angle)), 3);
             gr.FillRectangle(_brTan, currWidth / 2 - 180 * sin - width / 2, currHeight / 2 - 180  * cos - height / 2, width, height);
         }
 
@@ -98,7 +126,7 @@ namespace sin_cos_tan_simulation
                 System.Threading.Thread.Sleep(time);
             });
             thread.Start();
-            while (thread.IsAlive)
+            while (thread.IsAlive) { }
                 Application.DoEvents();
         }
     }
